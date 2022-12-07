1be0b7870d0ba1d2aa3d6919234a1bad143bf9c4
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-12-07 15:15:01 +0100

    update based on feedback

--- code/simple-app/server.js
+++ code/simple-app/server.js
@@ -4,6 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 var cors = require("cors");
 app.use(cors());
+var bodyParser = require('body-parser');
+app.use(bodyParser.json() );  
+app.use(bodyParser.urlencoded({extended: true}));
 
 const port = 3000;
 
@@ -13,11 +16,13 @@ const IamAuthenticator = require('ibm-watson/auth').IamAuthenticator;
 var e_version=process.env.WATSON_ASSISTANT_VERSION;
 var e_apikey=process.env.WATSON_ASSISTANT_API_KEY;
 var e_serverUrl=process.env.WATSON_ASSISTANT_SERVICE_URL;
-var e_assistantID=process.env.WASTON_ASSISTANT_ID;
+var e_assistantID=process.env.WATSON_ASSISTANT_ID;
+var e_environmentID=process.env.WATSON_ASSISTANT_ENVIRONMENT_ID;
 console.log(process.env.WATSON_ASSISTANT_VERSION);
 console.log(process.env.WATSON_ASSISTANT_API_KEY);
 console.log(process.env.WATSON_ASSISTANT_SERVICE_URL);
-console.log(process.env.WASTON_ASSISTANT_ID);
+console.log(process.env.WATSON_ASSISTANT_ID);
+console.log(process.env.WATSON_ASSISTANT_ENVIRONMENT_ID);
 
 const assistant = new AssistantV2({
   version: e_version,
@@ -28,22 +33,31 @@ const assistant = new AssistantV2({
 });
 
 
-app.get('/getSession', (req, res) => {
-    let sessionID;
-    assistant.createSession({
-        assistantId: e_assistantID
-    })
-    .then(res => {
-        console.log(JSON.stringify(res.result, null, 2));
-        sessionID = JSON.stringify(res.result, null, 2);
-        return sessionID;
-    })
-    .catch(err => {
-        console.log(err);
-        sessionID = err;
-        return sessionID;
-    });
+app.get('/getsession', (req, res) => {
+    var returnvalue;
+     
+    returnvalue = returnSessionID();
+    console.log("Return value : ", returnvalue);
+    res.statusCode = 200;
+    res.write(JSON.stringify(returnvalue));
+    res.end();    
 });
+
+function returnSessionID (){
+    let sessionID;
+    
+    assistant.createSession({
+            //assistantId: e_assistantID
+            assistantId: e_environmentID
+        })
+        .then(res => {
+            console.log(JSON.stringify(res.result, null, 2));
+            sessionID = JSON.stringify(res.result, null, 2);
+        })
+        .catch(err => {
+            console.log(err);   
+        });
+}
 
 const server = app.listen(port, function () {
     console.log('server is running on port:', port ); 
